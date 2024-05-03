111b55e6d3c0620ea46869ce4bcf50b608957c28
calvarado <cristian.alvarado@upre.gob.bo>
2024-05-03 01:31:39 -0400

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'json',
+    loadComponent:()=>import('./probando/login/login.component'),
+    children:[
+      {
+        path:'hijo',
+        loadComponent:()=>import('./probando/login/hijo/hijo.component'),
+      }
+    ]
+  },
+  {
+    path:'',
+    redirectTo:'/json',
+    pathMatch:'full'
+  }
+];
